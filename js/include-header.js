8057a57c18f4d68a87af1d7e4ea52ffb3e5b4b40
NaghiBogdan109 <gg.naghi@gmail.com>
2025-03-11 22:55:12 +0200

    Update include-header.js

--- js/include-header.js
+++ js/include-header.js
@@ -1,1 +1,10 @@
+// include-header.js
+document.addEventListener("DOMContentLoaded", function() {
+  fetch("header.html")   // dacă header.html e în aceeași rădăcină cu index.html
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("header-include").innerHTML = data;
+    })
+    .catch(err => console.error("Eroare la încărcarea header-ului:", err));
+});
 
