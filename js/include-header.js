cf94f051074c3d083e919e9106c10a9ab718c915
NaghiBogdan109 <gg.naghi@gmail.com>
2025-07-23 17:52:33 +0300

    Update include-header.js

--- js/include-header.js
+++ js/include-header.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
   let i18nPath   = "js/i18n.js";
 
   // 2) Dacă ești în /html/windows/ sau /html/office/, mergi două niveluri în sus
-  if (pathName.includes("/html/windows/") || pathName.includes("/html/office/")) || pathName.includes("/html/adobe/")) {
+  if (pathName.includes("/html/windows/") || pathName.includes("/html/office/")) {
     headerPath = "../../header.html";
     i18nPath   = "../../js/i18n.js";
   }
